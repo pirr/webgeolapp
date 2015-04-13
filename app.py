d07619675033347c7eec99e83e3d421a9d23bc5d
pirr <tattoo1mag@gmail.com>
2015-04-13 17:58:16 +0800

    search in progress

--- app.py
+++ app.py
@@ -2,24 +2,18 @@ from flask import Flask, render_template, request, session, jsonify, g, url_for,
 import importlib
 import pymysql
 import data.db_con as db
+import difflib
 
 app = Flask(__name__)
 dic_cur = db.dbCon().cursor(pymysql.cursors.DictCursor)
 
 users = [('one','pass1'),('two','pass2')]
 
-test_page = Blueprint('test_page', __name__,
-                        template_folder='templates')
-@app.route('/test/')
-@app.route('/test/<checkid>')
-def test(checkid):
-    checkid = checkid
-    return render_template('test.html', checkid=checkid)
-    # return render_template('test.html',
-    #     user=session.get('user'),
-    #     checkid=checkid)
-    # checkid=session.get('checkid')
-    # return render_template('test.html', checkid=checkid)
+# @app.route('/test/')
+# @app.route('/test/<checkid>')
+# def test(checkid):
+#     checkid = checkid
+#     return render_template('test.html', checkid=checkid)
 
 @app.route('/')
 def main():
@@ -59,17 +53,10 @@ def objs():
             'objects.html', 
             user=session.get('user')
             )
-@app.route('/checkdoc/', methods=['POST'])
-@app.route('/checkdoc/<check>')
-def checkdoc():
-    data = request.get_json()
-    session['checkid'] = data['check']
-    check = session['checkid']
-    return session['checkid']
 
-@app.route('/workspacedoc/<pick_id>')
-def workspacedoc(pick_id):
-    pick_id = pick_id
+@app.route('/objedit/<pick_id>')
+def objedit(pick_id):
+    session['pick_id'] = pick_id
     dic_cur.execute("""SELECT 
         objects.id, objects.obj_name, dic_doc_type.name AS 'doc_type',
         GROUP_CONCAT(dic_pi.pi ORDER BY dic_pi.pi SEPARATOR ', ') AS 'pi',
@@ -82,15 +69,49 @@ def workspacedoc(pick_id):
         LEFT JOIN dic_doc_type on dic_doc_type.id = obj_doc.doc_type_id
         WHERE objects.id = %s
         GROUP BY objects.id""", pick_id)
-    checkdoc = dic_cur.fetchone()
+    pickeddoc = dic_cur.fetchone()
+    session['pickedid'] = pick_id
+
     return render_template(
-            'workspacedoc.html', 
+            'objedit.html', 
             user=session.get('user'),
-            doc=checkdoc,
+            doc=pickeddoc,
+            matchdoc=session.get('matchdoc'),
             )
 
 
-        
+@app.route('/search', methods=['POST'])    
+def search():
+    data = request.get_json()
+    pick_id = session.get('pickedid')
+    
+    dic_cur.execute("""SELECT 
+            objects.id, objects.obj_name, dic_doc_type.name AS 'doc_type',
+            GROUP_CONCAT(dic_pi.pi ORDER BY dic_pi.pi SEPARATOR ', ') AS 'pi',
+            GROUP_CONCAT(DISTINCT dic_pi.type_pi ORDER BY dic_pi.type_pi 
+                SEPARATOR ', ') AS 'group_pi'
+            FROM objects
+            LEFT JOIN obj_pi on objects.id = obj_pi.obj_id 
+            LEFT JOIN dic_pi on dic_pi.id = obj_pi.pi_id
+            LEFT JOIN obj_doc on objects.id = obj_doc.obj_id
+            LEFT JOIN dic_doc_type on dic_doc_type.id = obj_doc.doc_type_id
+            WHERE objects.id <> %s
+            GROUP BY objects.id""", pick_id)
+    unpickeddata = dic_cur.fetchall()
+    
+    matchdoc = []
+    for doc in unpickeddata:
+        session['matchdoc'].clear()
+        matchdoc.clear()
+        if data['searchname'] in doc['obj_name']:
+            matchdoc.append(doc)
+            session['matchdoc'] = matchdoc
+            return 'Match'
+        else:
+            return 'Err'
+   
+    
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -108,6 +129,7 @@ def logout():
 if __name__ == '__main__':
     app.secret_key = "bacon"
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.debug = True
     app.run(
         debug=True, 
         host="0.0.0.0"
--- static/js/base.js
+++ static/js/base.js
@@ -6,7 +6,7 @@ function login() {
         url: '/login',
         data: JSON.stringify({user:user, password:password}),
         success: function(response) {
-            if (response=='Err');
+            if (response=='Err') 'Err';
             else location.reload(); 
         }
     })     
@@ -21,66 +21,21 @@ function logout() {
     });
 }
 
-<<<<<<< HEAD
-// function checkdoc(but) {
-//     var check = but.getAttribute('value');
-//     console.log(JSON.stringify({check:check}));
-//     $.ajax({
-//         url: '/checkdoc',
-//         data: JSON.stringify({check:check}),
-//         success: function(data) {
-//             return data;
-//         }
-//     });
-   
-// }
-=======
-function openurl(url) {
+function search() {
+    searchname = $('#searchname').val();
+    console.log(JSON.stringify({'searchname':searchname}))
+    $.ajax({
+        url: '/search',
+        data: JSON.stringify({'searchname': searchname}),
+        success: function(response) {
+            location.reload(); 
+        }
+    });
+}
+
+function open_url(url){
     window.open(url, '_blank');
 }
->>>>>>> 3dc9c0dd881b5af912ce4b0ec4562f0c2eb8b8a5
-
-$(document).ready(function($) {
-    $(".clickable-row").click(function() {
-        window.open($(this).data("href"), '_blank')
-    });
-});
-
-// $(document).ready(function() {
-//     $('button').click(function(){
-//             url: '/documents'
-//         var check = $(this).prop('value');
-//         $.ajax({
-//             url: '/checkdoc/',
-//             data: JSON.stringify({check:check}),
-//             success: function(data) {
-//             console.log(JSON.stringify({check:check}))
-//             return data;
-//         }
-//         });
-//     });
-//     });
-
-// $(document).ready( function() {
-//     $('#test').each(function(){
-//         $('#test').click(function(){
-//             console.log($(this).val());
-//     });
-// });
-// });
-
-//active class navbar button
-function openurl(url) {
-    window.open(url, '_blank')
-    }
-
-$(document).ready(function () {
-        var url = window.location;
-        $('ul.nav a[href="" + url + ""]').parent().addClass('active');
-        $('ul.nav a').filter(function() {
-             return this.href == url;
-        }).parent().addClass('active');
-    });
 
 $(document).ready( function() {
     jQuery.ajaxSetup( {
@@ -88,5 +43,10 @@ $(document).ready( function() {
         contentType: 'application/json',
         async: true, 
     });
-    
-});
+    //active class navbar button
+    var url = window.location;
+        $('ul.nav a[href="" + url + ""]').parent().addClass('active');
+        $('ul.nav a').filter(function() {
+             return this.href == url;
+        }).parent().addClass('active');
+    });
