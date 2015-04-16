b9474fec51d43649d4ab30e97f8c937bfcc3a80d
pirr <tattoo1mag@gmail.com>
2015-04-16 19:03:01 +0800

    add map(Leaflet) in doc.html

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def main():
             checkid=session.get('checkid')
             )
 
+
 @app.route('/documents_preview')
 def documents_preview():
     return render_template(
             'documents_preview.html',
             user=session.get('user')
             )
-
 
 @app.route('/documents', methods=['POST', 'GET'])
 def documents():
@@ -52,46 +52,72 @@ def documents():
     return jsonify(html=html)
 
 
-
 @app.route('/objs_preview')
 def objs_preview():
+    dic_cur = db.dbCon().cursor(pymysql.cursors.DictCursor)
+    
+    dic_cur.execute("""SELECT 
+        objects.obj_id, COUNT(*) AS docs_count,
+        GROUP_CONCAT(dic_pi.pi ORDER BY dic_pi.pi SEPARATOR ', ') AS 'pi',
+        GROUP_CONCAT(DISTINCT dic_pi.type_pi ORDER BY dic_pi.type_pi SEPARATOR ', ') AS 'group_pi'
+        FROM objects 
+        LEFT JOIN doc_pi ON objects.doc_id = doc_pi.doc_id 
+        LEFT JOIN dic_pi ON dic_pi.id = doc_pi.pi_id
+        WHERE objects.obj_id<>0 
+        GROUP BY objects.obj_id
+        """)
+    objs = dic_cur.fetchall()
+
     return render_template(
             'objs_preview.html',
+            objs = objs,
             user=session.get('user')
             )
 
-@app.route('/objects', methods=['POST'])
-def objs():
+@app.route('/object/<doc_id>', methods=['POST'])
+def obj(doc_id):
     dic_cur = db.dbCon().cursor(pymysql.cursors.DictCursor)
 
-    dic_cur.execute("""SELECT 
-        documents.id, objects.obj_id, documents.doc_name, dic_source_type.name AS 'source_type',
-        GROUP_CONCAT(dic_pi.pi ORDER BY dic_pi.pi SEPARATOR ', ') AS 'pi',
-        GROUP_CONCAT(DISTINCT dic_pi.type_pi ORDER BY dic_pi.type_pi SEPARATOR ', ') AS 'group_pi' 
-        FROM documents
-        LEFT JOIN objects ON documents.id = objects.doc_id
-        LEFT JOIN doc_pi ON documents.id = doc_pi.doc_id 
-        LEFT JOIN dic_pi ON dic_pi.id = doc_pi.pi_id
-        LEFT JOIN source ON documents.id = source.doc_id
-        LEFT JOIN dic_source_type ON dic_source_type.id = source.source_type_id
-        WHERE objects.obj_id <> 'None'
-        GROUP BY documents.id
-        """)
-    docs = dic_cur.fetchall()
-    docs = sorted(docs, key=lambda doc: int(doc['obj_id']))
+    dic_cur.execute("""SELECT
+        objects.obj_id FROM objects
+        WHERE objects.doc_id = %s""", doc_id)
+    picked_group_sql = dic_cur.fetchone()
 
-    html = render_template(
-            'docs.html',
-            docs=docs, 
-            )
+    if picked_group_sql:
+        obj_id = picked_group_sql['obj_id']
+
+        dic_cur.execute("""SELECT 
+            documents.id, objects.obj_id, documents.doc_name, dic_source_type.name AS 'source_type',
+            GROUP_CONCAT(dic_pi.pi ORDER BY dic_pi.pi SEPARATOR ', ') AS 'pi',
+            GROUP_CONCAT(DISTINCT dic_pi.type_pi ORDER BY dic_pi.type_pi SEPARATOR ', ') AS 'group_pi' 
+            FROM documents
+            LEFT JOIN objects ON documents.id = objects.doc_id
+            LEFT JOIN doc_pi ON documents.id = doc_pi.doc_id 
+            LEFT JOIN dic_pi ON dic_pi.id = doc_pi.pi_id
+            LEFT JOIN source ON documents.id = source.doc_id
+            LEFT JOIN dic_source_type ON dic_source_type.id = source.source_type_id
+            WHERE objects.obj_id = %s
+            GROUP BY documents.id
+            """, obj_id)
+        docs = dic_cur.fetchall()
+        docs = sorted(docs, key=lambda doc: int(doc['obj_id']))
+
+        html = render_template(
+                'docs.html',
+                docs=docs, 
+                )
+    else:
+        html = 'Нет группы'
 
     return jsonify(html=html)
+
 
 @app.route('/doc_about/<doc_id>')
 def doc_about(doc_id):
     return render_template(
             'doc_about.html',
-            doc_id=doc_id
+            doc_id=doc_id,
+            user=session.get('user')
             )
 
 @app.route('/doc_preview_edit/<doc_id>', methods=['POST'])
@@ -101,17 +127,19 @@ def doc_preview_edit(doc_id):
     dic_cur.execute("""SELECT 
         documents.id, objects.obj_id, documents.doc_name, dic_source_type.name AS 'source_type',
         GROUP_CONCAT(dic_pi.pi ORDER BY dic_pi.pi SEPARATOR ', ') AS 'pi',
-        GROUP_CONCAT(DISTINCT dic_pi.type_pi ORDER BY dic_pi.type_pi SEPARATOR ', ') AS 'group_pi' 
+        GROUP_CONCAT(DISTINCT dic_pi.type_pi ORDER BY dic_pi.type_pi SEPARATOR ', ') AS 'group_pi', doc_coordinates.lat, doc_coordinates.lon 
         FROM documents
         LEFT JOIN objects ON documents.id = objects.doc_id
         LEFT JOIN doc_pi ON documents.id = doc_pi.doc_id 
         LEFT JOIN dic_pi ON dic_pi.id = doc_pi.pi_id
         LEFT JOIN source ON documents.id = source.doc_id
         LEFT JOIN dic_source_type ON dic_source_type.id = source.source_type_id
+        LEFT JOIN doc_coordinates ON documents.id = doc_coordinates.doc_id
         WHERE documents.id = %s ##choose doc
         GROUP BY documents.id
         """, doc_id)
     doc = dic_cur.fetchone()
+
     
     html = render_template(
             'doc.html',
@@ -119,6 +147,8 @@ def doc_preview_edit(doc_id):
             )
 
     return jsonify(html=html)
+
+
 
 # @app.route('/obj_edit/<pick_id>')
 # def objedit(pick_id):
