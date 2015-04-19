455b1c8eb9e1bb6dd223b4824ecc9c59b6873823
pirr <tattoo1mag@gmail.com>
2015-04-19 14:05:59 +0800

    re factoring

--- static/js/base.js
+++ static/js/base.js
@@ -33,47 +33,11 @@ function search() {
     });
 }
 
-<<<<<<< HEAD
-// $(document).ready(function() {
-//     $('button').click(function(){
-//             url: '/documents'
-//         var check = $(this).prop('value');
-//         $.ajax({
-//             url: '/checkdoc/',
-//             data: JSON.stringify({check:check}),
-//             success: function(data) {
-//             console.log(JSON.stringify({check:check}))
-//             return data;
-//         }
-//         });
-//     });
-//     });
+// function open_url(url){
+//     window.open(url, '_blank');
+// }
 
-// $(document).ready( function() {
-//     $('#test').each(function(){
-//         $('#test').click(function(){
-//             console.log($(this).val());
-//     });
-// });
-// });
-
-
-
-//active class navbar button
-$(document).ready(function () {
-        var url = window.location;
-        $('ul.nav a[href="" + url + ""]').parent().addClass('active');
-        $('ul.nav a').filter(function() {
-             return this.href == url;
-        }).parent().addClass('active');
-    });
-=======
-function open_url(url){
-    window.open(url, '_blank');
-}
->>>>>>> 751610120d0f3ff750ecbc9101bb2ca2a5007739
-
-function docs_in_objs_response(url, id){
+function prepare_view(url, id){
 $(document).ready( function() {
     $.ajax({
         url: url,
